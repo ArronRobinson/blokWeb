ffe1d659533737dd784c0c12ba60366da62eebaa
ArronRobinson <90252558+ArronRobinson@users.noreply.github.com>
2022-09-26 17:11:53 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,9 @@
 // JavaScript Document
-console.log("hi");
+var buttonMenu = document.querySelector("nav button:nth-of-type(3)");
+var subMenu = document.querySelector("main ul");
+
+buttonMenu.addEventListener("click", toggleMenu);
+
+function toggleMenu () {
+    subMenu.classList.toggle("open");
+}
